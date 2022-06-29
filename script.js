928e63592c13c5a6fb10f2166ece4c85c9efc198
Filipe Garrote <filipe.garrote@gmail.com>
2022-06-29 02:39:09 -0300

    v2 - Layout + renderização das imagens

---
+++ script.js
@@ -0,0 +1,28 @@
+const quizzesContainer = document.querySelector(".all-quizzes");
+
+getQuizzes()
+
+function getQuizzes(){
+  const promise = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes")
+  promise.catch((error) => {
+    alert("Ocorreu um erro inesperado. A página será recarregada.");
+    window.location.reload();
+  })
+  promise.then(renderQuizzes)
+}
+
+function renderQuizzes(success){
+  let APIQuizzes = success.data;
+  quizzesContainer.innerHTML = null;
+  for (let i = 0; i < APIQuizzes.length; i++){
+    quizzesContainer.innerHTML += `
+    <div>
+      <img src=${APIQuizzes[i].image}>
+      <h3>${APIQuizzes[i].title}</h3>
+      <div class="opacity"></div>
+    </div>`
+  }
+}
+
+
+
